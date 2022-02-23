44febad531cbd1a9bf2da219cbf3fe4561482f2c
Jamboy <jamboylive@gmail.com>
2022-02-23 10:54:33 +0800

    video网络请求封装

--- pages/video/video.js
+++ pages/video/video.js
@@ -2,7 +2,7 @@
  * @Description: 视频
  * @Author: Jamboy
  * @Date: 2022-02-22 09:41:35
- * @LastEditTime: 2022-02-22 18:15:01
+ * @LastEditTime: 2022-02-23 10:50:30
  */
 // pages/video/video.js
 
@@ -13,17 +13,31 @@ Page({
      * 页面的初始数据
      */
     data: {
-        topMvs: []
+        topMvs: [],
+        hasMore: true
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     async onLoad(options) {
-        const res = await getTopMvs(0)
-        console.log('res: ', res.data[0].mv.videos[0].duration)
-        this.setData({ topMvs: res.data })
+        this.getTopMvData(0)
+    },
 
+    async getTopMvData(offset) {
+        wx.showNavigationBarLoading()
+        const { hasMore } = this.data
+        if (!hasMore && offset !== 0) { return }
+
+        const res = await getTopMvs(offset)
+        let newData = this.data.topMvs
+        if (offset === 0) {
+            newData = res.data
+        } else {
+            newData = newData.concat(res.data)
+        }
+        this.setData({ topMvs: newData, hasMore: res.hasMore })
+        wx.hideNavigationBarLoading()
     },
 
     onClickItem(e) {
@@ -61,21 +75,15 @@ Page({
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {
-
+    async onPullDownRefresh() {
+        this.getTopMvData(0)
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function () {
-
+    async onReachBottom() {
+        this.getTopMvData(this.data.topMvs.length)
     },
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
 })
